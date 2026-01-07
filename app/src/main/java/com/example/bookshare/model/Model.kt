6eb120e0e4c5df2c61b1a19934f2f0ee64f581e9
Osher <osherzohar80@gmail.com>
2026-01-07 19:43:19 +0200

    add students MutableList logic

---
+++ app/src/main/java/com/example/bookshare/model/Model.kt
@@ -0,0 +1,52 @@
+package com.example.bookshare.model
+
+class Model private constructor() {
+
+    private val students: MutableList<Student> = ArrayList()
+
+    companion object {
+        val shared = Model()
+    }
+
+    init {
+        // Adding some dummy data for testing
+        students.add(Student(id = "123456", name = "Alice Smith", checkStatus = false))
+        students.add(Student(id = "789012", name = "Bob Jones", checkStatus = true))
+        students.add(Student(id = "345678", name = "Charlie Brown", checkStatus = false))
+    }
+
+    fun getAllStudents(): List<Student> {
+        return students
+    }
+
+    fun addStudent(student: Student) {
+        students.add(student)
+    }
+
+    fun getStudentById(id: String): Student? {
+        return students.find { it.id == id }
+    }
+
+    fun updateStudent(oldId: String, updatedStudent: Student) {
+        val index = students.indexOfFirst { it.id == oldId }
+        if (index != -1) {
+            students[index] = updatedStudent
+        }
+    }
+    
+    fun deleteStudent(id: String) {
+         val index = students.indexOfFirst { it.id == id }
+         if (index != -1) {
+             students.removeAt(index)
+         }
+    }
+
+    fun updateStudentStatus(id: String, isChecked: Boolean) {
+        val index = students.indexOfFirst { it.id == id }
+        if (index != -1) {
+            val student = students[index]
+            val newStudent = student.copy(checkStatus = isChecked)
+            students[index] = newStudent
+        }
+    }
+}
