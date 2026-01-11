ba10226ecc28c7a74887d6ef9fa8f73e718b0142
Osher <osherzohar80@gmail.com>
2026-01-11 21:42:04 +0200

    complete list conversion to fragment

--- app/src/main/java/com/example/bookshare/StudentListFragment.kt
+++ app/src/main/java/com/example/bookshare/StudentListFragment.kt
@@ -4,15 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookshare.adapter.StudentsAdapter
+import com.example.bookshare.model.Model
 
 class StudentListFragment : Fragment() {
+
+    private lateinit var studentsRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_student_list, container, false)
+
+        studentsRecyclerView = view.findViewById(R.id.studentsRecyclerView)
+        studentsRecyclerView.setHasFixedSize(true)
+        studentsRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Refresh the list
+        val adapter = StudentsAdapter(Model.shared.getAllStudents())
+        adapter.onStudentClick = { student ->
+             // Milestone 2.3: We will add Navigation here later
+             Toast.makeText(context, "Clicked on ${student.name}", Toast.LENGTH_SHORT).show()
+        }
+        studentsRecyclerView.adapter = adapter
     }
 }
