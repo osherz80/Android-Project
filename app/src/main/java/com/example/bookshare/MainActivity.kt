1776d6ab8e7886f1b92ef202e594b6c8bfc593f5
Osher <osherzohar80@gmail.com>
2026-01-11 21:39:50 +0200

    replace student list activity with fragment list

--- app/src/main/java/com/example/bookshare/MainActivity.kt
+++ app/src/main/java/com/example/bookshare/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.bookshare
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.bookshare.adapter.StudentsAdapter
-import com.example.bookshare.model.Model
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var studentsRecyclerView: RecyclerView
-    private lateinit var addStudentFab: FloatingActionButton
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,29 +27,12 @@ class MainActivity : AppCompatActivity() {
         val toolbar: com.google.android.material.appbar.MaterialToolbar = findViewById(R.id.mainToolbar)
         setSupportActionBar(toolbar)
 
-        studentsRecyclerView = findViewById(R.id.studentsRecyclerView)
-        addStudentFab = findViewById(R.id.addStudentFab)
-
-        studentsRecyclerView.setHasFixedSize(true)
-        studentsRecyclerView.layoutManager = LinearLayoutManager(this)
-        
-        // In the future (Milestone 6), we should move this to onResume to refresh the list
-        
-        addStudentFab.setOnClickListener {
-            val intent = Intent(this, AddStudentActivity::class.java)
-            startActivity(intent)
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    override fun onResume() {
-        super.onResume()
-        // Refresh the list
-        val adapter = StudentsAdapter(Model.shared.getAllStudents())
-        adapter.onStudentClick = { student ->
-             val intent = Intent(this, StudentDetailsActivity::class.java)
-             intent.putExtra("student_id", student.id)
-             startActivity(intent)
-        }
-        studentsRecyclerView.adapter = adapter
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
