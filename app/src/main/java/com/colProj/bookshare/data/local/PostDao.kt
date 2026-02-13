4a423b8674ca4acd3cec74088ec2427f95f2ca40
Assaf Revacha <assaf.git.arm@gmail.com>
2026-02-13 20:00:47 +0200

    add book details logic to post dao

--- app/src/main/java/com/colProj/bookshare/data/local/PostDao.kt
+++ app/src/main/java/com/colProj/bookshare/data/local/PostDao.kt
@@ -12,6 +12,12 @@ interface PostDao {
     @Query("SELECT * FROM posts ORDER BY timestamp DESC")
     fun getAllPosts(): LiveData<List<Post>>
 
+    @Query("SELECT * FROM posts WHERE bookTitle = :title ORDER BY timestamp DESC")
+    fun getPostsByTitle(title: String): LiveData<List<Post>>
+
+    @Query("SELECT * FROM posts WHERE userId = :userId ORDER BY timestamp DESC")
+    fun getUserPosts(userId: String): LiveData<List<Post>>
+
     @Query("SELECT * FROM posts WHERE bookTitle LIKE '%' || :query || '%' OR author LIKE '%' || :query || '%' OR userName LIKE '%' || :query || '%' ORDER BY timestamp DESC")
     fun searchPosts(query: String): LiveData<List<Post>>
 
